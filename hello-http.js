80f2f2e807423e850db5b72ce187d95d0eac1283
David McDonald <idavidmcdonald@me.com>
2017-01-22 11:50:55 +0000

    move things into functions

--- hello-http.js
+++ hello-http.js
@@ -7,45 +7,23 @@ var app = express()
 var uuid = '8492e75f4fd6469db132043fe94921d8';
 var major = 0; // 0 - 65535
 var minor = 0; // 0 - 65535
+var beaconTypeToSound = [
+    	"gate_in.wav",
+    	"stairs_up.wav",
+    	"stairs_down.wav",
+    	"flat_bit.wav",
+    	"lift.wav",
+    	"escalator.wav",
+    	"end_of_stairs_escalator.wav",
+    	"notice.wav",
+    	"destination.wav",
+    	"error.mp3"
+    ]
 
-app.get('/', function (req, res) {
-  var distance;
-  var playSound = true;
-  Bleacon.startScanning([uuid]);
-  // Bleacon.startScanning([uuid], [major], [minor]);
+function getVolumeFromDistance (distance) {
+	var volume;
 
-  Bleacon.on('discover', function(bleacon) {
-    distance = bleacon['accuracy'];
-
-    var beaconType = 1
-
-    // if (distance < .5) {
-    //   var beaconType = beaconType + 1;
-    // }
-
-    if (beaconType == 1 ) {
-      var object = "gate_in.wav"
-    } else if (beaconType == 2){
-      var object = "stairs_up.wav"
-    } else if (beaconType == 3){
-      var object = "stairs_down.wav"
-    } else if (beaconType == 4){
-      var object = "flat_bit.wav"
-    } else if (beaconType == 5){
-      var object = "lift.wav"
-    } else if (beaconType == 6){
-      var object = "escalator.wav"
-    } else if (beaconType == 7){
-      var object = "end_of_stairs_escalator.wav"
-    } else if (beaconType == 8){
-      var object = "notice.wav"
-    } else if (beaconType == 9){
-      var object = "destination.wav"
-    } else if (beaconType == 10){
-      var object = "error.mp3"
-    }
-
-    if (distance <= .1){
+	if (distance <= .1){
       var volume = 1
     } else if (distance <= .2){
       var volume = .9
@@ -67,17 +45,42 @@ app.get('/', function (req, res) {
       var volume = .1
     }
 
-    if (playSound) {
-    	player.play('./sounds/'+object, { afplay: ['-v', volume ] /* lower volume for afplay on OSX */ }, function(err){
-    	});
-    	playSound = false;
-    }
-    
-    console.log(bleacon['uuid']);
+    return volume
+}
+
+function playSound (file, volume) {
+	player.play('./sounds/' + file, { afplay: ['-v', volume ] /* lower volume for afplay on OSX */ }, function(err){});
+}
+
+function log (bleacon, distance, volume) {
+	console.log(bleacon['uuid']);
     console.log(bleacon['proximity'])
     console.log(bleacon['rssi']);
     console.log('>>>>>>>>>>>>');
     console.log('distance is ' + distance + ', volume is ' + volume);
+}
+
+app.get('/', function (req, res) {
+  var volume;
+  var distance;
+  var play = true;
+  Bleacon.startScanning([uuid]);
+  // Bleacon.startScanning([uuid], [major], [minor]);
+
+  Bleacon.on('discover', function(bleacon) {
+    distance = bleacon['accuracy'];
+
+    var beaconType = 1
+
+    object = beaconTypeToSound[beaconType - 1];
+
+    volume = getVolumeFromDistance(distance)
+    if (play) {
+    	playSound(object, volume);
+    	play = false;
+    }
+
+    log(bleacon, distance, volume)
   	res.end('distance is ' + distance + ', volume is ' + volume)
   })
 })
